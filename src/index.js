a1e954a3e91c4fbebe651e619e0654f07fc27105
Tobias Herber <40146720150275@litec.ac.at>
2018-02-17 17:43:14 +0100

    add main file for electron

---
+++ src/index.js
@@ -0,0 +1,34 @@
+'use strict';
+const electron = require('electron');
+const isDev = require('electron-is-dev');
+const window = require('./window');
+
+require('events').EventEmitter.prototype._maxListeners = 100;
+
+const app = electron.app;
+
+if (isDev) {
+  require('electron-debug')();
+}
+
+let mainWindow;
+
+function onClosed() {
+  mainWindow = null;
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (!mainWindow) {
+    mainWindow = window(onClosed);
+  }
+});
+
+app.on('ready', () => {
+  mainWindow = window(onClosed);
+});
