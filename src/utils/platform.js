be47ab9580cb6ef30b055bb4e183012e77e3069f
Tobias Herber <40146720150275@litec.ac.at>
2018-02-17 17:46:39 +0100

    add util for getting platform

---
+++ src/utils/platform.js
@@ -0,0 +1,12 @@
+module.exports = () => {
+  switch (process.platform) {
+    case 'darwin':
+      return 'macos';
+    case 'win32':
+      return 'windows';
+    case 'linux':
+      return 'linux';
+    default:
+      return 'none';
+  };
+};
