a35238ce83e7ed5a88543849c6fb71948d1ced25
Tobias Herber <40146720150275@litec.ac.at>
2018-02-18 22:07:59 +0100

    add tabs db

--- src/view.js
+++ src/view.js
@@ -2,6 +2,7 @@ const path = require('path');
 module.paths.push(path.resolve('../node_modules'));
 
 const mitt = require('mitt');
+const dexie = require('dexie');
 
 const webview = require('./view/webview');
 const keyboard = require('./view/keyboard');
@@ -21,7 +22,6 @@ titlebar(emitter, state);
 progress(emitter);
 history(emitter);
 webview(emitter, state);
-emitter.emit('webview-create');
 menu(emitter, state);
 keyboard(emitter, state);
 
@@ -30,3 +30,37 @@ setTimeout(() => {
 }, 400);
 
 document.querySelector('.urlbar').focus();
+
+let db = new dexie('tabs');
+db.version(1).stores({
+  tabRestore: 'url'
+});
+
+db.tabRestore.count((count) => {
+  if (count == 0) {
+    emitter.emit('webview-create');
+  } else {
+    db.tabRestore.each(view => {
+      emitter.emit('webview-create', view.url);
+    });
+  }
+})
+
+setInterval(() => {
+  db.delete().then(() => {
+    try {
+      db = new dexie('tabs');
+      db.version(1).stores({
+        tabRestore: 'url'
+      });
+    } catch (err) {}
+
+    for (let view of state.views) {
+      db.tabRestore.put({ url: document.querySelector('#' + view.id).getURL(), title: document.querySelector('#' + view.id).getTitle() });
+    }
+  });
+}, 500);
+
+emitter.on('tabs-db-flush', () => {
+  db.delete();
+})
