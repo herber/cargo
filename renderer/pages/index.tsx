337548ba9687bec52a848a92acf0399bea9cf184
Tobias Herber <tobias@herber.space>
2019-10-01 11:06:44 +0200

    Set up renderer

---
+++ renderer/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { NextPage } from 'next';
+
+let IndexPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello Cargo 👋</h1>
+    </div>
+  )
+};
+
+export default IndexPage;
